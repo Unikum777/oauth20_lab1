199eb6e1a3a6cef4b93d06c8c79d78feddcc17ad
Yershov Nike <yershov.n@mail.ru>
2014-11-23 21:51:08 +0300

    SimpleOauthConnect

---
+++ WebApplication1/WebApplication1/Default.aspx
@@ -0,0 +1,11 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebApplication1._Default" %>
+
+<asp:Content runat="server" ID="BodyContent" ContentPlaceHolderID="MainContent">
+    <asp:Label ID="Label1" runat="server" Text="login"></asp:Label>
+    <br><asp:TextBox ID="Login" runat="server">yershov.n@outlook.com</asp:TextBox></br>
+    <asp:Label ID="Label2" runat="server" Text="Password"></asp:Label>
+    <br><asp:TextBox ID="Password" runat="server">GregToBack94$</asp:TextBox></br>
+    <br><asp:Button ID="Button1" runat="server" Text="Sign in" OnClick="Button1_Click" /></br>
+    
+    <br> <asp:Label ID="Label3" runat="server" Text="Label"></asp:Label></br>
+    </asp:Content>
---
+++ WebApplication1/WebApplication1/Default.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace WebApplication1
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            HttpClient client = new HttpClient();
+            //FormUrlEncodedContent form = new FormUrlEncodedContent(new Dictionary<string, string> { { "username", Login.Text }, { "password", Password.Text } });
+            FormUrlEncodedContent form = new FormUrlEncodedContent(new Dictionary<string, string> { 
+            { "client_id", "000000004C12F126" }, 
+            { "scope", "huh9wvXlYFQ94hJ198i-7bOnBn46oi4a" }, 
+            { "redirect_uri", "http://localhost:45226/Default.aspx"} });
+
+            var message = client.PostAsync("https://login.live.com/oauth20_authorize.srf", form);
+            String result = message.Result.Content.ReadAsStringAsync().Result;
+
+            Label3.Text = result;
+            /*Newtonsoft.Json.Linq.JObject obj = Newtonsoft.Json.Linq.JObject.Parse(result);
+            string token = (string)obj["access_token"];
+            string refreshToken = (string)obj["refresh_token"];*/
+        }
+    }
+}
